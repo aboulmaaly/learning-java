e22f79ec61df372b40c707d7df3f4413b1ac2489
Aboulmaaly Mohamed <maboulmaaly@yahoo.com>
2020-06-08 14:20:45 +0100

    Create JavaVariables Class ...

---
+++ variables/src/JavaVariables.java
@@ -0,0 +1,41 @@
+/**
+ * Variables
+ *
+ * The Java programming language defines the following kinds of variables:
+ *
+ *    - Instance Variables (Non-Static Fields) :
+ *			Fields declared without the static keyword.
+ *
+ *    - Class Variables (Static Fields) :
+ *			A class variable is any field declared with the static modifier.
+ *
+ *	  - Local Variables :
+ *			A method will often store its temporary state in local variables.
+ *
+ *    - Parameters :
+ *			The important thing to remember is that parameters are always classified as "variables" not "fields".
+ *
+ * Naming Variables
+ *
+ *	  Every programming language has its own set of rules and conventions for the kinds of names that you're allowed to use, 
+ *    and the Java programming language is no different.
+ *
+ *    The rules and conventions for naming your variables can be summarized as follows:
+ *		
+ *			- Variable names are case-sensitive. 
+ *			- A variable's name can be any sequence of Unicode letters and digits.
+ *			- Beginning with a letter, the dollar sign "$", or the underscore character "_".
+ *			- The convention, however, is to always begin your variable names with a letter, not "$" or "_".
+ *			- Subsequent characters may be letters, digits, dollar signs, or underscore characters.
+ *			- When choosing a name for your variables, use full words instead of cryptic abbreviations.
+ *			- If the name you choose consists of only one word, spell that word in all lowercase letters.
+ *			- If it consists of more than one word, capitalize the first letter of each subsequent word.
+ *			- If your variable stores a constant value, the convention changes slightly, 
+ *				capitalizing every letter and separating subsequent words with the underscore character.
+ * */
+public class JavaVariables {
+	
+	public static void main(String[] args) {
+		
+	}
+}
