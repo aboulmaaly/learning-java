5cdeefa9aba4eb5418c61f9bd304122a15f8cf1a
Aboulmaaly Mohamed <maboulmaaly@yahoo.com>
2020-06-08 14:46:46 +0100

    Create JavaHelloWorld Class ...

---
+++ introduction/src/JavaHelloWorld.java
@@ -0,0 +1,19 @@
+/**
+ * It's time to write your first application!
+ *
+ * The JavaHelloWorld class implements an application that
+ * simply prints "Hello World!" to standard output.
+ *
+ * @author Aboulmaaly
+ */
+public class JavaHelloWorld {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // Display the string
+        System.out.println("Hello World!");
+        // Output : Hello World!
+    }
+}
