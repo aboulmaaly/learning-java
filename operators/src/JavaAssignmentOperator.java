b00adbc513d6c58f4529f4dfadf65cbcf800891e
Aboulmaaly Mohamed <maboulmaaly@yahoo.com>
2020-06-09 14:28:59 +0100

    Create JavaAssignmentOperator class ...

---
+++ operators/src/JavaAssignmentOperator.java
@@ -0,0 +1,14 @@
+/**
+ * Assignment Operator
+ *
+ * One of the most common operators that you'll encounter is the simple assignment operator "=".
+ * It assigns the value on its right to the operand on its left.
+ * This operator can also be used on objects to assign object references.
+ */
+public class JavaAssignmentOperator {
+    public static void main(String[] args) {
+        int x = 5;
+        char letter = 'A';
+        String hello = "Hello World!";
+    }
+}
